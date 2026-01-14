905a73e97058d8569bd2e851f266a6b46b61a11f
samik117 <stelsjet117@gmail.com>
2026-01-14 12:46:10 +0300

    First commit

--- Vaulin/Lesson1.py
+++ Vaulin/Lesson1.py
@@ -6,3 +6,25 @@ population = 13000000
 population_2 = str(population)
 
 print("Город: "+city+","+"Население: "+population_2)
+
+#Задача 2
+price = 19.99
+
+quantity = 3
+
+total_cost = price * quantity
+
+print(total_cost)
+
+#Задача 3
+visota = 15
+
+shirina = "42"
+
+shirina_2 = int(shirina)
+
+perimetr = visota + shirina_2
+
+print(perimetr, type(perimetr))
+
+
