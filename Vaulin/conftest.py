12af78f98c631411cfba45b18997819cf021f330
samik117 <stelsjet117@gmail.com>
2026-04-19 16:18:29 +0300

    Practice

--- Vaulin/conftest.py
+++ Vaulin/conftest.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 from selenium import webdriver
 
@@ -7,3 +8,15 @@ def driver(request):
     request.cls.driver = driver
     yield
     driver.quit()
+
+@pytest.fixture(autouse=True)
+def setup_environment_properties():
+    properties = {
+        "STAGE": os.environ["STAGE"],
+        "BROWSER": os.environ["BROWSER"],
+        "MR": os.environ["MR"],
+        "PYTHON": os.environ["PYTHON"],
+    }
+    with open("allure-results/environment.properties", "w") as file:
+        for key, value in properties.items():
+            file.write(f"{key}={value}\n")
